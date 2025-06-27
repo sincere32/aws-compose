ac23611f0d48ae3564855ee198624d91be34b96c
SiNCeRe <sincere_por@hotmail.com>
2025-06-27 21:36:37 +0700

    aa

---
+++ freeradius/opt/freeradius/mysql/initdb.d/radius.sql
@@ -0,0 +1,267 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 172.100.0.101
+-- Generation Time: May 22, 2018 at 12:23 PM
+-- Server version: 5.7.22
+-- PHP Version: 7.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `radius`
+--
+CREATE DATABASE IF NOT EXISTS `radius` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `radius`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admins`
+--
+
+CREATE TABLE IF NOT EXISTS `admins` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(30) NOT NULL,
+  `firstname` varchar(30) NOT NULL,
+  `lastname` varchar(30) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `admins`
+--
+
+INSERT INTO `admins` (`id`, `username`, `firstname`, `lastname`, `password`) VALUES
+(1, 'admin', 'Default', 'Admin', '5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `groups`
+--
+
+CREATE TABLE IF NOT EXISTS `groups` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) NOT NULL,
+  `description` varchar(80) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `groups`
+--
+
+INSERT INTO `groups` (`id`, `name`, `description`) VALUES
+(1, 'staff', NULL),
+(3, 'engineers', NULL),
+(5, 'default', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `nas`
+--
+
+CREATE TABLE IF NOT EXISTS `nas` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `nasname` varchar(128) CHARACTER SET latin1 NOT NULL,
+  `shortname` varchar(32) CHARACTER SET latin1 DEFAULT NULL,
+  `type` varchar(30) CHARACTER SET latin1 DEFAULT 'other',
+  `ports` int(5) DEFAULT NULL,
+  `secret` varchar(60) CHARACTER SET latin1 NOT NULL DEFAULT 'secret',
+  `server` varchar(64) CHARACTER SET latin1 DEFAULT NULL,
+  `community` varchar(50) CHARACTER SET latin1 DEFAULT NULL,
+  `description` varchar(200) CHARACTER SET latin1 DEFAULT 'RADIUS Client',
+  PRIMARY KEY (`id`),
+  KEY `nasname` (`nasname`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `radacct`
+--
+
+CREATE TABLE IF NOT EXISTS `radacct` (
+  `radacctid` bigint(21) NOT NULL AUTO_INCREMENT,
+  `acctsessionid` varchar(64) CHARACTER SET latin1 NOT NULL DEFAULT '',
+  `acctuniqueid` varchar(32) CHARACTER SET latin1 NOT NULL DEFAULT '',
+  `username` varchar(64) CHARACTER SET latin1 NOT NULL DEFAULT '',
+  `groupname` varchar(64) CHARACTER SET latin1 NOT NULL DEFAULT '',
+  `realm` varchar(64) CHARACTER SET latin1 DEFAULT '',
+  `nasipaddress` varchar(15) CHARACTER SET latin1 NOT NULL DEFAULT '',
+  `nasportid` varchar(15) CHARACTER SET latin1 DEFAULT NULL,
+  `nasporttype` varchar(32) CHARACTER SET latin1 DEFAULT NULL,
+  `acctstarttime` datetime DEFAULT NULL,
+  `acctupdatetime` datetime DEFAULT NULL,
+  `acctstoptime` datetime DEFAULT NULL,
+  `acctinterval` int(12) DEFAULT NULL,
+  `acctsessiontime` int(12) UNSIGNED DEFAULT NULL,
+  `acctauthentic` varchar(32) CHARACTER SET latin1 DEFAULT NULL,
+  `connectinfo_start` varchar(50) CHARACTER SET latin1 DEFAULT NULL,
+  `connectinfo_stop` varchar(50) CHARACTER SET latin1 DEFAULT NULL,
+  `acctinputoctets` bigint(20) DEFAULT NULL,
+  `acctoutputoctets` bigint(20) DEFAULT NULL,
+  `calledstationid` varchar(50) CHARACTER SET latin1 NOT NULL DEFAULT '',
+  `callingstationid` varchar(50) CHARACTER SET latin1 NOT NULL DEFAULT '',
+  `acctterminatecause` varchar(32) CHARACTER SET latin1 NOT NULL DEFAULT '',
+  `servicetype` varchar(32) CHARACTER SET latin1 DEFAULT NULL,
+  `framedprotocol` varchar(32) CHARACTER SET latin1 DEFAULT NULL,
+  `framedipaddress` varchar(15) CHARACTER SET latin1 NOT NULL DEFAULT '',
+  PRIMARY KEY (`radacctid`),
+  UNIQUE KEY `acctuniqueid` (`acctuniqueid`),
+  KEY `username` (`username`),
+  KEY `framedipaddress` (`framedipaddress`),
+  KEY `acctsessionid` (`acctsessionid`),
+  KEY `acctsessiontime` (`acctsessiontime`),
+  KEY `acctstarttime` (`acctstarttime`),
+  KEY `acctinterval` (`acctinterval`),
+  KEY `acctstoptime` (`acctstoptime`),
+  KEY `nasipaddress` (`nasipaddress`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `radcheck`
+--
+
+CREATE TABLE IF NOT EXISTS `radcheck` (
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `username` varchar(64) CHARACTER SET latin1 NOT NULL DEFAULT '',
+  `attribute` varchar(64) CHARACTER SET latin1 NOT NULL DEFAULT '',
+  `op` char(2) CHARACTER SET latin1 NOT NULL DEFAULT '==',
+  `value` varchar(253) CHARACTER SET latin1 NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`),
+  KEY `username` (`username`(32))
+) ENGINE=InnoDB AUTO_INCREMENT=480 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `radcheck`
+--
+
+INSERT INTO `radcheck` (`id`, `username`, `attribute`, `op`, `value`) VALUES
+(478, 'testuser', 'SHA-Password', ':=', '0f47d148a94d7c492327aee00986b24503b0667b'),
+(479, 'testuser', 'Simultaneous-Use', ':=', '5');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `radgroupcheck`
+--
+
+CREATE TABLE IF NOT EXISTS `radgroupcheck` (
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `groupname` varchar(64) CHARACTER SET latin1 NOT NULL DEFAULT '',
+  `attribute` varchar(64) CHARACTER SET latin1 NOT NULL DEFAULT '',
+  `op` char(2) CHARACTER SET latin1 NOT NULL DEFAULT '==',
+  `value` varchar(253) CHARACTER SET latin1 NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`),
+  KEY `groupname` (`groupname`(32))
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `radgroupreply`
+--
+
+CREATE TABLE IF NOT EXISTS `radgroupreply` (
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `groupname` varchar(64) CHARACTER SET latin1 NOT NULL DEFAULT '',
+  `attribute` varchar(64) CHARACTER SET latin1 NOT NULL DEFAULT '',
+  `op` char(2) CHARACTER SET latin1 NOT NULL DEFAULT '=',
+  `value` varchar(253) CHARACTER SET latin1 NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`),
+  KEY `groupname` (`groupname`(32))
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `radpostauth`
+--
+
+CREATE TABLE IF NOT EXISTS `radpostauth` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(64) CHARACTER SET latin1 NOT NULL DEFAULT '',
+  `pass` varchar(64) CHARACTER SET latin1 NOT NULL DEFAULT '',
+  `reply` varchar(32) CHARACTER SET latin1 NOT NULL DEFAULT '',
+  `authdate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `radreply`
+--
+
+CREATE TABLE IF NOT EXISTS `radreply` (
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `username` varchar(64) CHARACTER SET latin1 NOT NULL DEFAULT '',
+  `attribute` varchar(64) CHARACTER SET latin1 NOT NULL DEFAULT '',
+  `op` char(2) CHARACTER SET latin1 NOT NULL DEFAULT '=',
+  `value` varchar(253) CHARACTER SET latin1 NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`),
+  KEY `username` (`username`(32))
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `radusergroup`
+--
+
+CREATE TABLE IF NOT EXISTS `radusergroup` (
+  `username` varchar(64) NOT NULL DEFAULT '',
+  `groupname` varchar(64) NOT NULL DEFAULT '',
+  `priority` int(11) NOT NULL DEFAULT '1',
+  KEY `username` (`username`(32))
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `radusergroup`
+--
+
+INSERT INTO `radusergroup` (`username`, `groupname`, `priority`) VALUES
+('testuser', 'default', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(40) NOT NULL,
+  `firstname` varchar(50) NOT NULL,
+  `lastname` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `firstname`, `lastname`) VALUES
+(8, 'testuser', 'Test', 'User');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
